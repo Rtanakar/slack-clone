b59744ccbe2ce807a4990c4a9ae9041f22b2249b
Rtanakar <ratnakarmishra@623gmail.com>
2024-09-07 10:48:49 +0530

    workspace join page 1

---
+++ src/app/join/[workspaceId]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import Image from "next/image";
+import Link from "next/link";
+import VerificationInput from "react-verification-input";
+
+function JoinPage() {
+  const workspaceId = useWorkspaceId();
+
+  return (
+    <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
+      <Image src="/hashtag.jpg" width={60} height={60} alt="hashtag" />
+      <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
+        <div className="flex flex-col gap-y-2 items-center justify-center">
+          <h1 className="text-2xl font-bold">Join workspace</h1>
+          <p className="text-base text-muted-foreground">
+            Enter the workspace code to join
+          </p>
+        </div>
+        <VerificationInput
+          length={6}
+          classNames={{
+            container: "flex gap-x-2",
+            character:
+              "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black",
+          }}
+          autoFocus
+        />
+      </div>
+      <div className="flex gap-x-4">
+        <Button size="lg" variant="outline" asChild>
+          <Link href="/">Back to home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default JoinPage;
